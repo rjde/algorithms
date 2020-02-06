a6697d5cae08e6693aa761f2905307bd401fadb9
rjde <rjuidev@gmail.com>
2020-02-06 21:28:16 +0000

    critical connections update

--- src/algorithms/criticalConnections.js
+++ src/algorithms/criticalConnections.js
@@ -6,18 +6,49 @@ function criticalConnections(numberOfNodes, connections) {
   const disc = new Array(numberOfNodes);
   disc.fill(-1);
   const result = [];
-
+  
   dfs(0, low, disc, result, graph, 0);
-
+  
   return result;  
 }
 
 function dfs(current, low, disc, result, graph, parent) {
+  low[current] = disc[current] = ++id;
 
+  for(let i=0; i<graph[current].length; i++) {
+    const next = graph[current][i];
+    if(next === parent) {
+      continue;
+    }
+    if(disc[next] === -1) {
+      dfs(next, low, disc, result, graph, current);
+      low[current] = Math.min(low[current], low[next]);
+      if(disc[current] < low[next]) {
+        result.push([current, next]);
+      }
+    } else {
+      low[current] = Math.min(low[current], disc[next]);
+    }
+  }
 }
 
 function buildGraph(connections) {
+  const graph = {};
 
+  for(let i=0; i<connections.length; i++) {
+    const [first, second] = connections[i];
+    if(!(first in graph)) {
+      graph[first] = [];
+    }
+    if(!(second in graph)) {
+      graph[second] = [];
+    }
+
+    graph[first].push(second);
+    graph[second].push(first);
+  }
+
+  return graph;
 }
 
 module.exports = criticalConnections;
