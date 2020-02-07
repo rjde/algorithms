af02979eb236f2bde5f3abe6d30a49866020b4a9
rakeshadloori <rjuidev@gmail.com>
2020-02-06 22:23:37 -0800

    formatted

--- src/algorithms/criticalConnections.js
+++ src/algorithms/criticalConnections.js
@@ -6,24 +6,24 @@ function criticalConnections(numberOfNodes, connections) {
   const disc = new Array(numberOfNodes);
   disc.fill(-1);
   const result = [];
-  
+
   dfs(0, low, disc, result, graph, 0);
-  
-  return result;  
+
+  return result;
 }
 
 function dfs(current, low, disc, result, graph, parent) {
   low[current] = disc[current] = ++id;
 
-  for(let i=0; i<graph[current].length; i++) {
+  for (let i = 0; i < graph[current].length; i++) {
     const next = graph[current][i];
-    if(next === parent) {
+    if (next === parent) {
       continue;
     }
-    if(disc[next] === -1) {
+    if (disc[next] === -1) {
       dfs(next, low, disc, result, graph, current);
       low[current] = Math.min(low[current], low[next]);
-      if(disc[current] < low[next]) {
+      if (disc[current] < low[next]) {
         result.push([current, next]);
       }
     } else {
@@ -35,12 +35,12 @@ function dfs(current, low, disc, result, graph, parent) {
 function buildGraph(connections) {
   const graph = {};
 
-  for(let i=0; i<connections.length; i++) {
+  for (let i = 0; i < connections.length; i++) {
     const [first, second] = connections[i];
-    if(!(first in graph)) {
+    if (!(first in graph)) {
       graph[first] = [];
     }
-    if(!(second in graph)) {
+    if (!(second in graph)) {
       graph[second] = [];
     }
 
